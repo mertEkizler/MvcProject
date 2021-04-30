3e2f1ab580bc811b2c72056328a32541d8b21c58
mert ekizler <mertekizler@hotmail.com>
2021-04-30 20:51:18 +0300

    refactor

--- MvcProject/Controllers/CategoryController.cs
+++ MvcProject/Controllers/CategoryController.cs
@@ -19,8 +19,8 @@ namespace MvcProject.Controllers
         }
         public ActionResult GetCategoryList()
         {
-            var categoryValues = cm.GetAllBL();
-            return View(categoryValues);
+            //var categoryValues = cm.GetAllBL();
+            return View();
 
         }
 
@@ -33,7 +33,7 @@ namespace MvcProject.Controllers
         [HttpPost]
         public ActionResult AddCategory(Category p)
         {
-            cm.CategoryAddBL(p);
+           // cm.CategoryAddBL(p);
             return RedirectToAction("GetCategoryList");
         }
     }
