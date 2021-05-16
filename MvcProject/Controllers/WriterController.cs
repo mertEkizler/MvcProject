c7c03931e8cb2a6428483cea5ce6f415269a5d58
mert ekizler <mertekizler@hotmail.com>
2021-05-16 23:01:10 +0300

    ME- Yeni yazar ekleme yapıldı.

--- MvcProject/Controllers/WriterController.cs
+++ MvcProject/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +20,33 @@ namespace MvcProject.Controllers
         {
             var WriterValues = wm.GetList();
             return View(WriterValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator writeValidator = new WriterValidator();
+            ValidationResult results = writeValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
