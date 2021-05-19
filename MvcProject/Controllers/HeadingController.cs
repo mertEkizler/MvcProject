58aa81dd871e7dbcd94bf39703873991af30c86d
mert ekizler <mertekizler@hotmail.com>
2021-05-19 15:37:28 +0300

    ME-HeadingController düzenlendi

--- MvcProject/Controllers/HeadingController.cs
+++ MvcProject/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,34 @@ namespace MvcProject.Controllers
     {
         // GET: Heading
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
 
         public ActionResult Index()
         {
             var headingValues = hm.GetList();
             return View(headingValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valueCategory;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse( DateTime.Now.ToShortDateString());
+
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
