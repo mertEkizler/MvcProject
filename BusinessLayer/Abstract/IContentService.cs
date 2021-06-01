4eb08ee60eaa7aef135053fad977d206c9b7c37f
mert ekizler <mertekizler@hotmail.com>
2021-06-01 20:20:47 +0300

    ME-ContentManager eklendi

---
+++ BusinessLayer/Abstract/IContentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IContentService
+    {
+        List<Content> GetList();
+        void ContentAddBL(Content content);
+        Content GetByID(int id);
+        void ContentDelete(Content content);
+        void ContentUpdate(Content content);
+        List<Content> GetListByID(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/ContentManager.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContentManager : IContentService
+    {
+        IContentDal _contentDal;
+
+        public ContentManager(IContentDal contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public void ContentAddBL(Content content)
+        {
+            _contentDal.Insert(content);
+        }
+
+        public void ContentDelete(Content content)
+        {
+            _contentDal.Delete(content);
+            
+        }
+
+        public void ContentUpdate(Content content)
+        {
+            _contentDal.Update(content);
+        }
+
+        public Content GetByID(int id)
+        {
+            return _contentDal.Get(co => co.ContentID == id);
+        }
+
+        public List<Content> GetList()
+        {
+            return _contentDal.List();
+        }
+
+        public List<Content> GetListByID(int id)
+        {
+            return _contentDal.List(co => co.HeadingID == id);
+        }
+    }
+}
