6ba898269a37e32bcdac908cf7917d8e20c291e9
mert ekizler <mertekizler@hotmail.com>
2021-06-03 23:12:19 +0300

    ME-Mail işlemleri düzenlendi

---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,36 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı adresini boş geçemezsiniz.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu adını boş geçemezsiniz.");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı boş geçemezsiniz");
+            RuleFor(x => x.ReceiverMail).Must(IsRealMailAdress).WithMessage("Lütfen geçerli bir mail giriniz");
+            RuleFor(x => x.SenderMail).Must(IsRealMailAdress).WithMessage("Lütfen geçerli bir mail giriniz");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen 3 karakterden az değer girişi yapmayınız.");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayınız.");
+        }
+
+        private bool IsRealMailAdress(string mail)
+        {
+            if (mail.EndsWith("com"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
